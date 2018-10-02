63413b3ec578eb1404bae10762cbbcbb5a6abc48
Victor Romero <victor.romero2004@gmail.com>
2018-10-02 23:18:48 +0000

    Helloworld complette

--- 00_hello/hello.rb
+++ 00_hello/hello.rb
@@ -1,1 +1,17 @@
 #write your code here
+def hello(name)
+    if name == "Victor"
+        puts "Hello #{name}"
+    else
+        puts "hello World..!!"
+    end
+end
+
+def main
+    print "What is your name: "
+    name = gets.chomp
+    hello(name)
+end
+
+
+main if __FILE__ ==$PROGRAM_NAME
