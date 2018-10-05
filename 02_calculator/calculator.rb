d087be3120786ede7a0ea766acbbf7cef3ed0925
Victor Romero <victor.romero2004@gmail.com>
2018-10-05 01:29:39 +0000

    calculator ver2.0

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,10 +1,11 @@
 #write your code here
-
-def zum(num_zum)
-    zum_array.new
-    zum_array << num_zum
-    puts num_zum
+def zum(num1)
+    zum_array = num1.to_a
+    zum = zum_array.sum
+    puts zum
 end
+
+
 
 def dif(num_dif)
 end
@@ -15,7 +16,11 @@ end
 def div(num_div)
 end
 
-def factorial (num_fact)
+def factorial(number)
+    number = number.to_i
+    number_range = (number).downto(1).to_a
+    factorial = number_range.inject(:*)
+    puts "The factorial of #{number} is #{factorial}"
 end
 
 def power(num_power)
@@ -26,55 +31,74 @@ end
 def main
     puts "Ruby's Calculator"
     puts " Use (+) to sum // (-) to rest // (*) to multiply // (/) to Divide // (**) for power // (!) for Factorial "
-    number1 = gets.chomp  
-    sum = number1.to_i
-    dif = number1.to_i
-    prod = number1.to_i
+    number1 = gets.chomp.to_i  
+    sum = number1
+    dif = number1
+    prod = number1
     div = number1.to_f
-    pow = number1.to_i
+    pow = number1
+    fac = number1
     operator = gets.chomp
-#    while operator == "+" || operator == "-" || operator == "*" || operator == "/" ||
-    
-        if operator == "+"
-            number2 = gets.chomp
-            sum = sum + number2.to_i
-            operator = gets.chomp
-            puts "total: #{sum}" if operator != "+"
+
+       if   operator == "+"
+           loop do 
+                number2 = gets.chomp!
+                sum = sum + number2.to_i
+                operator = gets.chomp!
+                if operator == "="
+                    puts "total: #{sum}"
+                    break
+                end
+            end
+            
+       elsif operator == "-"
+            loop do
+                number2 = gets.chomp
+                dif = dif-number2.to_i
+                operator = gets.chomp
+                if operator != "-"
+                    puts "total: #{dif}" 
+                    break
+                end
+            end 
         
-        
-        elsif operator == "-"
-            number2 = gets.chomp
-            dif = dif-number2.to_i
-            operator = gets.chomp
-            puts "total: #{dif}" if operator != "-"
-        
-        
-        elsif operator == "*"
-            number2 = gets.chomp
-            prod = prod*number2.to_i
-            operator = gets.chomp
-            puts "total: #{prod}" if operator != "*"
+       elsif operator == "*"
+            loop do
+                number2 = gets.chomp
+                prod = prod*number2.to_i
+                operator = gets.chomp
+                if operator != "*"
+                    puts "total: #{prod}"
+                    break
+                end
+            end
         
             
         elsif operator == "/"
-            number2 = gets.chomp
-            div = div/number2.to_f
-            operator = gets.chomp
-            puts "total: #{div}" if operator != "/"
-        
+            loop do
+                number2 = gets.chomp
+                div = div/number2.to_f
+                operator = gets.chomp
+                if operator != "/"    
+                    puts "total: #{div}" 
+                    break
+                end
+            end
+            
         elsif operator == "**"
-            number2 = gets.chomp
-            pow = pow**number2.to_i
-            operator = gets.chomp
-            puts "total: #{pow}" if operator != "**"
+            loop do
+                number2 = gets.chomp
+                pow = pow**number2.to_i
+                operator = gets.chomp
+                if operator != "**"
+                    puts "total: #{pow}"
+                    break
+                end
+            end
+            
+        elsif operator == "!"
+            factorial(number1)
         end
-#        operator = gets.chomp
-#    end
-        
-        
-    
-    
-    
 end
 
 main if __FILE__ ==$PROGRAM_NAME
